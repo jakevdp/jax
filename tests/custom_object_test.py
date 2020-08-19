a0755a07183c1691d5a275cf8f0a12d15c6daea6
Jake VanderPlas <jakevdp@google.com>
2020-08-19 13:03:24 -0700

    add primitives for buffer access

--- tests/custom_object_test.py
+++ tests/custom_object_test.py
@@ -31,8 +31,8 @@ class SparseArray:
   def __init__(self, aval, shape, data, indices):
     self.aval = aval
     self.shape = shape
-    self.data = data
-    self.indices = indices
+    self._data = data
+    self._indices = indices
 
   @property
   def index_dtype(self):
@@ -46,41 +46,51 @@ class SparseArray:
   def nnz(self):
     return self.data.shape[0]
 
+  @property
+  def data(self):
+    return self._data
+
+  @property
+  def indices(self):
+    return self._indices
+
   def __repr__(self):
     return repr(list((tuple(ind), d) for ind, d in zip(self.indices, self.data)))
 
 
 class AbstractSparseArray(core.ShapedArray):
-  __slots__ = ['index_dtype', 'nnz']
+  __slots__ = ['index_dtype', 'nnz', '_data', '_indices']
   _num_buffers = 2
 
   def __init__(self, shape, dtype, index_dtype, nnz):
     super(AbstractSparseArray, self).__init__(shape, dtype)
     self.index_dtype = index_dtype
     self.nnz = nnz
+    self._data = core.ShapedArray((self.nnz,), self.dtype)
+    self._indices = core.ShapedArray((self.nnz, len(self.shape)), self.index_dtype)
 
-  @property
+  @core.aval_property
   def data(self):
-    return core.ShapedArray((self.nnz,), self.dtype)
+    return sp_data_p.bind(self)
 
-  @property
+  @core.aval_property
   def indices(self):
-    return core.ShapedArray((self.nnz, len(self.shape)), self.index_dtype)
+    return sp_indices_p.bind(self)
 
 def abstract_sparse_array(arr):
   return AbstractSparseArray(arr.shape, arr.dtype, arr.index_dtype, arr.nnz)
 
 def sparse_array_result_handler(device, aval):
   def build_sparse_array(data_buf, indices_buf):
-    data = xla.DeviceArray(aval.data, device, lazy.array(aval.data.shape), data_buf)
-    indices = xla.DeviceArray(aval.indices, device, lazy.array(aval.indices.shape), indices_buf)
+    data = xla.DeviceArray(aval._data, device, lazy.array(aval._data.shape), data_buf)
+    indices = xla.DeviceArray(aval._indices, device, lazy.array(aval._indices.shape), indices_buf)
     return SparseArray(aval, aval.shape, data, indices)
   return build_sparse_array
 
 def sparse_array_shape_handler(a):
   return (
-    xla.xc.Shape.array_shape(a.data.dtype, a.data.shape),
-    xla.xc.Shape.array_shape(a.indices.dtype, a.indices.shape),
+    xla.xc.Shape.array_shape(a._data.dtype, a._data.shape),
+    xla.xc.Shape.array_shape(a._indices.dtype, a._indices.shape),
   )
 
 def sparse_array_device_put_handler(a, device):
@@ -97,6 +107,36 @@ xla.device_put_handlers[SparseArray] = sparse_array_device_put_handler
 xla.xla_result_handlers[AbstractSparseArray] = sparse_array_result_handler
 xla.xla_shape_handlers[AbstractSparseArray] = sparse_array_shape_handler
 
+
+sp_indices_p = core.Primitive('sp_indices')
+
+@sp_indices_p.def_impl
+def _sp_indices_impl(mat):
+  return mat.indices
+
+@sp_indices_p.def_abstract_eval
+def _sp_indices_abstract_eval(mat):
+  return core.ShapedArray((mat.nnz, len(mat.shape)), mat.index_dtype)
+
+def _sp_indices_translation_rule(c, data, indices):
+  return indices
+
+xla.translations[sp_indices_p] = _sp_indices_translation_rule
+
+sp_data_p = core.Primitive('sp_data')
+
+@sp_data_p.def_impl
+def _sp_data_impl(mat):
+  return mat.data
+
+@sp_data_p.def_abstract_eval
+def _sp_data_abstract_eval(mat):
+  return core.ShapedArray((mat.nnz,), mat.dtype)
+
+def _sp_data_translation_rule(c, data, indices):
+  return data
+
+xla.translations[sp_data_p] = _sp_data_translation_rule
 
 def identity(x):
   return identity_p.bind(x)
@@ -153,6 +193,21 @@ class CustomObjectTest(jtu.JaxTestCase):
     body_fun = lambda _, A: f(A)
     lax.fori_loop(0, 10, body_fun, make_sparse_array())
 
+  @parameterized.named_parameters(jtu.cases_from_list(
+      {"testcase_name": "_attr={}".format(attr), "attr": attr}
+      for attr in ["data", "indices"]))
+  def testAttrAccess(self, attr):
+    args_maker = lambda: [make_sparse_array()]
+    f = lambda x: getattr(x, attr)
+    self._CompileAndCheck(f, args_maker)
+
 
 if __name__ == '__main__':
   absltest.main(testLoader=jtu.JaxTestLoader())
+
+  # from jax import core
+  # core.skip_checks = False
+  # def f(x): return (x.indices, x.data)
+  # M = make_sparse_array()
+  # print(f(M))
+  # print(jit(f)(M))
